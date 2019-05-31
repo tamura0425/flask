a4192e46d58df7fbe810ad0ee1d8d60a6c79e29b
Your Name <you@example.com>
2019-05-31 16:56:37 +0900

    gitもだいぶなれてきた

---
+++ input.java
@@ -0,0 +1,7 @@
+public class Min {
+	public static void(String[] args){
+		int ans = 300 + 2000;
+
+		System.out.println(ans);
+	}
+}
---
+++ output.java
@@ -0,0 +1,8 @@
+import java.util.*
+
+public static class Ans{
+	public static void (String args[]]{
+			
+	}
+
+}
