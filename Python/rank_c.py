7434d12e608cb2411cfac1b316a366d900fb814b
“hirotada tamura <tamura0425@gmail.com>
2019-08-24 23:44:07 +0900

    paizaランクｃ足すか掛けるか　8月24日クリアー

---
+++ Python/rank_c.py
@@ -0,0 +1,16 @@
+time = int(input())
+result = 0
+
+num = int(input()) #ループ回数
+sum = 0 # 合計
+
+for i in range(num):
+    inputNum = input()
+    array = inputNum.split()
+
+    if array[0] == array[1]:
+        sum += int(array[0]) * int(array[1])
+    else:
+        sum += int(array[0]) + int(array[1])
+
+print(sum)
