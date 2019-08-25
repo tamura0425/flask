d3c32c3131db8d94f2744e5894419af96c7a3253
“hirotada tamura <tamura0425@gmail.com>
2019-08-25 21:57:46 +0900

    8月25日　Paiza 字列の分割 STEP: 3　をクリアー

---
+++ Python/STEP_ 3.py
@@ -0,0 +1,7 @@
+# 文字列がスペース区切りで2つ入力されるので、スペースで分割し、2行で出力してください。
+
+num = input()
+
+for array in num.split():
+    print(array)
+
--- java/Paiza.java
+++ java/Paiza.java
@@ -2,8 +2,19 @@ import java.util.*;
 
 public class Paiza {
     public static void main(String[] args) throws Exception {
-        // Your code here!
-        
-        System.out.println("XXXXXXXX");
-    }
+		Scanner sc = new Scanner(System.in);
+		
+		
+        int loop = sc.nextInt();
+        sc.nextLine();//行おくり
+		for(int i = 0; i  <= loop ; i++){
+			String str = sc.nextLine();
+			System.out.println(str.trim());
+		}
+		
+	}
 }
+
+
+
+
