07688c8ac146d3ec024da89a45164a623a99668c
“hirotada tamura <tamura0425@gmail.com>
2019-08-27 02:40:47 +0900

    8月27に paiza 4-4 「あいだの整数 Python3編」  クリアー

---
+++ Python/step4-2.py
@@ -0,0 +1,14 @@
+disp = input().split(' ')
+
+disp1 = int(disp[0])
+disp2 = int(disp[1])
+
+for i in range(disp1, (disp2+1)):
+    print(i)
+
+
+# 下記の表記でもＯＫ
+# nums = input().split()
+
+# for i in range(int(nums[0]), int(nums[1]) + 1):
+#     print(i)
