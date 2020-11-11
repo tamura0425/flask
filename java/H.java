4596fd37dc230f99074fed64c9638b81c8a29898
“hirotada tamura <tamura0425@gmail.com>
2020-11-11 21:36:20 +0900

    test test test

--- java/H.java
+++ java/H.java
@@ -7,12 +7,23 @@ public class H{public static void main(String[] args){
     int output1 = sc.nextInt();
 
     int output2 = sc.nextInt();
+    System.out.println(ans(output1,output2));
     int outputAns = ans(output1,output2);
-    System.out.println(outputAns);
+    System.out.println(outputAns); 
+   
+    ans1(output1,output2);
+  
   }
+  
   public static int ans(int a,int b){
     int back = a * b;
     return back;
   }
+public static void ans1(int a , int b){
+   System.out.println(a+b);
+}
+
+
+
 }
 
