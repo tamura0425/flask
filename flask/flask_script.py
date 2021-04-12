502c6418341094be78a1b553dbfecd773c411e3c
hirotada_Flask <tiancunhongzhong@gmail.com>
2021-04-12 14:39:57 +0900

    勉強あるのみ！

--- flask/flask_script.py
+++ flask/flask_script.py
@@ -1,5 +1,8 @@
 from flask import *  # 必要なライブラリのインポート
 
+# render_templateを使用するために記載
+from flask import Flask, render_template #追加
+
 app = Flask(__name__)  # アプリの設定 ・Flaskオブジェクトの生成
 
 
@@ -9,16 +12,16 @@ def main():      #メイン関数
 
 
 
-#@app.route("/<name>")
-#def hello_name(name):
-#    return "Hello, {}".format(name)
+@app.route("/<name>")
+def hello_name(name):
+    return "Hello, {}".format(name)
 
 
 
 
-@app.route("/name")
-def hello_name():
-    return "5555555555"
+#@app.route("/name")
+#$def hello_name():
+#    return "5555555555"
 
 
 @app.route("/index")
@@ -26,6 +29,19 @@ def index():
     return render_template("index.html")
 
 
+
+@app.route("/111")
+def hello_world():
+   #変数定義
+    name = "Flask"
+#templateのデータを渡すためにrender_templateの引数として記述[ name_value = name]
+    return render_template("index.html", name_value = name)
+
+
+@app.route("/222")
+def test():
+   #変数定義
+    return render_template("layout.html")
 
 
 
--- myapp/bbs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'bbs'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:id>', views.detail, name='detail'),
-]
