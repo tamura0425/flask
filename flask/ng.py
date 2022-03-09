b4bf541039a99683c7ffcdc492ee14f4e1841681
“hirotada tamura <tamura0425@gmail.com>
2022-03-09 15:42:29 +0900

    Pythonファイルの整理

--- flask/ng.py
+++ flask/ng.py
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
-=======
 # 必要なライブラリのインポート
 from flask import *
 from flask import Flask
->>>>>>> c59828f18874d9f15882b51a04f89706843e5d8c
+
 from flask import Blueprint
 
 ng_module = Blueprint("ng", __name__)
