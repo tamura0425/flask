2b4f4e1c8955b2dc8ebfe9e3b1d885526dfc9ca2
“hirotada tamura <tamura0425@gmail.com>
2022-03-29 00:43:28 +0900

    error 修正

--- ok.py
+++ ok.py
@@ -36,13 +36,13 @@ def sake_type():
 #      return render_template("Sake_type.html")
 #script.js
 
-@ok_module.route("/js/1")
-def jstest1():
+# @ok_module.route("/js/1")
+# def jstest1():
     #変数定義
     # return render_template("55test.html")
 
-@ok_module.route("/js/2")
-def jstest2():
+# @ok_module.route("/js/2")
+# def jstest2():
     #変数定義
     # return render_template("test.html")
 
